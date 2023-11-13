790eb2517707a75f7b70d6bda48f140121b45e2f
MinhPD <minh.pd210586@sis.hust.edu.vn>
2023-11-13 21:01:48 +0700

    new design for login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import "../css/LoginForm.css"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/LoginForm.css'
+
+import { HiOutlineKey } from 'react-icons/hi2'
+import { AiOutlineUser } from 'react-icons/ai'
+
+import LayoutImage from '../img/japan-vintage.jpg'
+var sectionStyle = {
+    backgroundImage: `url(${LayoutImage})`,
+    height: `100vh`,
+    
+}
 
 export function LoginPage() {
     const BASE_URL = 'http://127.0.0.1:8000';
@@ -61,16 +73,62 @@ export function LoginPage() {
     }
 
     return (
-        <>
-            {groupid !== null ? null :
-                <div className="Loginform">
-                    <input className="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} /><br></br>
-                    <input className="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} /><br></br>
-
-                    <button className="SignIn" onClick={SignIn}>Login</button>
-                    <button className="SignUp" onClick={SignUp}>Sign Up</button>
+        <div className='bg'>
+            <div className="Auth-form-container">
+                <div className='support-layout'>
+                    <h6 className='support-title'>Nếu cần hỗ trợ kỹ thuật, vui lòng thực hiện một trong các cách sau: </h6>
+                    <ol className='support-content'>
+                        <li>Gửi mail cho phòng kỹ thuật <strong style={{ color: 'red' }}>phongkythuat@gmail.com</strong></li>
+                        <li>Gọi số Hotline của phòng kỹ thuật <strong style={{ color: 'red' }}>012345678</strong></li>
+                    </ol>
                 </div>
-            }
-        </>
-    )
+                <br />
+                <form className="Auth-form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Đăng Nhập</h3>
+                        <div className="form-group mt-3">
+                            <label>Tài khoản</label>
+
+                            <div className='input-group mb-3'>
+                                <div className='input-group-prepend'>
+                                    <span className='input-group-text'>
+                                        <AiOutlineUser size={28} />
+                                    </span>
+                                </div>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nhập tên tài khoản"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Mật khẩu</label>
+
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text"> <HiOutlineKey size={28} /> </span>
+                                </div>
+
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Nhập mật khẩu"
+                                />
+                            </div>
+                        </div>
+                        <div className="d-grid gap-2 mt-3">
+                            <button type="submit" className="btn btn-primary">
+                                Đăng nhập
+                            </button>
+                        </div>
+                        {/* <p className="forgot-password text-right mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p> */}
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
