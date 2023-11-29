d4407fbdddfd1856a5b3372270c803d65c9e2893
Nhật Minh Hoàng <nhatminhhoang@Nhats-MacBook-Pro.local>
2023-11-29 13:44:49 +0700

    update weekly 1

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "../css/AdminForm.css";
 import AdminNavBar from "../components/AdminNavBar";
 import AdminContent from "../components/AdminContent";
 
