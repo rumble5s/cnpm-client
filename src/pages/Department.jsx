4da5ab584a66d51f7ed956ffd247d35455cd4002
Nhật Minh Hoàng <minh.hn215426@sis.hust.edu.vn>
2023-12-19 08:22:59 +0700

    update search bar

--- src/pages/Department.jsx
+++ src/pages/Department.jsx
@@ -117,6 +117,10 @@ export const Department = () => {
         <main className="content px-3 py-2">
           <h2>
             <strong>Danh sách căn hộ</strong>
+            <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
           </h2>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
